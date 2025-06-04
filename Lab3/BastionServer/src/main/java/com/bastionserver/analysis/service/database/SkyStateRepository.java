9b5e76d562494cd3cb5fbb34ebd9e22fa31e7ade
NureSynenkoIvan <103214936+IvanIKS@users.noreply.github.com>
2025-06-04 12:38:26 +0300

    deleted server

--- Lab3/BastionServer/src/main/java/com/bastionserver/analysis/service/database/SkyStateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bastionserver.analysis.service.database;
-
-import com.bastionserver.analysis.model.SkyState;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkyStateRepository extends MongoRepository<SkyState,String> {
-}
